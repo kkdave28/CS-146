cc1ba725aa5780bfac0f8db4c14a1c796682ffbb
kkdave28 <kkdave@uci.edu>
2018-05-10 14:46:10 -0700

    C-interp created and works

---
+++ Assignment-3/eg.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("testing C-Interp\n");
+    printf("Arg-1 = %s\n", argv[1]);
+    printf("Arg-2 = %s\n", argv[2]);
+    printf("Arg-3 = %s\n", argv[3]);
+    return 0;
+}
